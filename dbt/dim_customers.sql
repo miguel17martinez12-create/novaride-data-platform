4f41d24317737b064ed9ecc6f0235cdf6cfe799b
miguel17martinez12-create <miguel17martinez12@gmail.com>
2026-08-20 09:39:16 -0500

    Create dim_customers.sql

---
+++ dbt/dim_customers.sql
@@ -0,0 +1,11 @@
+select
+    customer_id,
+    full_name,
+    email,
+    country_code,
+    city,
+    customer_status,
+    signup_channel,
+    created_at,
+    updated_at
+from novaride.silver.customers
