dd783ba688c9581d35b2a24de21897c9e83905e4
miguel17martinez12-create <miguel17martinez12@gmail.com>
2026-08-20 09:56:43 -0500

    Update fct_trips.sql

--- dbt/models/fct_trips.sql
+++ dbt/models/fct_trips.sql
@@ -1,26 +1,43 @@
 select
-    trip_id,
-    customer_id,
-    driver_id,
-    vehicle_id,
-    city,
-    trip_status,
-    currency,
+    t.trip_id,
+    t.customer_id,
+    c.country_code,
+    c.city as customer_city,
 
-    requested_at,
-    accepted_at,
-    pickup_at,
-    completed_at,
+    t.driver_id,
+    t.vehicle_id,
 
-    distance_km,
-    duration_minutes,
-    base_fare,
-    surge_multiplier,
-    discount_amount,
-    tax_amount,
-    total_amount,
+    t.city as trip_city,
+    t.trip_status,
+    t.currency,
 
-    created_at,
-    updated_at
+    t.requested_at,
+    t.accepted_at,
+    t.pickup_at,
+    t.completed_at,
 
-from novaride.silver.trips
+    t.distance_km,
+    t.duration_minutes,
+
+    t.base_fare,
+    t.surge_multiplier,
+    t.discount_amount,
+    t.tax_amount,
+    t.total_amount,
+
+    p.payment_id,
+    p.payment_method,
+    p.payment_status,
+    p.processor_fee,
+    p.paid_at,
+
+    t.created_at,
+    t.updated_at
+
+from novaride.silver.trips t
+
+left join novaride.silver.payments p
+    on t.trip_id = p.trip_id
+
+left join novaride.silver.customers c
+    on t.customer_id = c.customer_id
